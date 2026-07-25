57bef71f6e597de46bd62e3525ac07980b715877
lakshyatatoo <lakshyatatoo31@gmail.com>
2026-07-25 12:18:09 +0530

    chore: Remove .env file containing sensitive configuration details

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
